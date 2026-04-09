5102f910dd8e88c1df3abe38a07d161fe56f41f8
neeharikavg <153418502+neeharikavg@users.noreply.github.com>
2026-04-09 23:08:58 +0530

    commit with sample cdoe

---
+++ header.js
@@ -0,0 +1,10 @@
+let Header = () => {
+    return (
+        <div> 
+            <div className="logo">
+                <img src="./Free-Logos-PNG-Picture.png" alt="Stylized header logo" />
+            </div>
+        </div>
+    )
+};
+export default Header;
